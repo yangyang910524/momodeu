00a1ab07f18b143748c45aa1645593297ede9636
yangyang910524 <383871887@qq.com>
2019-05-06 16:08:50 +0800

    接口

--- src/main/java/com/jeeplus/modules/notice/entity/Notice.java
+++ src/main/java/com/jeeplus/modules/notice/entity/Notice.java
@@ -4,8 +4,11 @@
 package com.jeeplus.modules.notice.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.jeeplus.core.persistence.DataEntity;
 import com.jeeplus.common.utils.excel.annotation.ExcelField;
+
+import java.util.Date;
 
 /**
  * 消息公告Entity
@@ -18,8 +21,37 @@ public class Notice extends DataEntity<Notice> {
 	private String title;		// 标题
 	private String content;		// 内容
 	private String state;		// 状态
-	
-	public Notice() {
+    private Date issueTime;		// 发布时间
+    private Date beginIssueTime;		// 开始 发布时间
+    private Date endIssueTime;		// 结束 发布时间
+
+
+    public Date getBeginIssueTime() {
+        return beginIssueTime;
+    }
+
+    public void setBeginIssueTime(Date beginIssueTime) {
+        this.beginIssueTime = beginIssueTime;
+    }
+
+    public Date getEndIssueTime() {
+        return endIssueTime;
+    }
+
+    public void setEndIssueTime(Date endIssueTime) {
+        this.endIssueTime = endIssueTime;
+    }
+
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    public Date getIssueTime() {
+        return issueTime;
+    }
+
+    public void setIssueTime(Date issueTime) {
+        this.issueTime = issueTime;
+    }
+
+    public Notice() {
 		super();
 	}
 
